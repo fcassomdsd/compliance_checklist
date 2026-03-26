27032159e98f50fbbf91c9a3eb1b27359b69eec4
Fernando Casso <fernando.casso27@gmail.com>
2026-03-26 18:37:33 -0400

    fix: corrected checklist id pattern - corrected checklist ID pattern to only accept 4 initial alphabetic characters, not alphanumeric. - corrected tests to concur with this pattern.

--- electron/__tests__/ipcHandles.test.js
+++ electron/__tests__/ipcHandles.test.js
@@ -410,7 +410,7 @@ describe('ipcHandles', () => {
       })
 
       const checklist = {
-        inspection: 'MDSD-2026-01',
+        inspection: '0224',
         providerId: 'provider-1',
         locationId: 'loc-1',
         location: 'Test Location',
@@ -455,7 +455,7 @@ describe('ipcHandles', () => {
       })
 
       const checklist = {
-        inspection: 'MDSD-2026-01',
+        inspection: '0224',
         providerId: 'provider-1',
         locationId: 'loc-1',
         location: 'Test Location',
